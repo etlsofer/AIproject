2376fcab93744ba118aa8c5d9645960d97fb876b
Etiel Sofer <50843359+etlsofer@users.noreply.github.com>
2020-08-03 12:37:02 +0300

    Add files via upload
    Adding data and script that unit all other data to one data

---
+++ PreData.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import sklearn
+import math
+
+NUM_OF_FEATURE = 16
+
+#transport dic to dataframe
+def ListToDF(dic):
+    list_header = ["num of events type "+ i +" for size 1" for i in range(NUM_OF_FEATURE)]
+    list_header2 = ["num of events type " + i + " for size 2" for i in range(NUM_OF_FEATURE)]
+
+#prepare Data
+def EventToDF(Data:pd.DataFrame):
+    event1 = [0 for i in range(NUM_OF_FEATURE)]
+    event2 = [0 for i in range(NUM_OF_FEATURE)]
+    for raw in Data.iterrows():
+        if raw[1]["side"] == 1:
+            event1[raw[1]["event_type"]] += 1
+            if math.isnan(raw[1]["event_type2"]) == False:
+                event1[int(raw[1]["event_type2"])] += 1
+        else:
+            event2[raw[1]["event_type"]] += 1
+            if math.isnan(raw[1]["event_type2"]) == False:
+                event2[int(raw[1]["event_type2"])] += 1
+    #print("team 1 have event:\n{}\nTeam 2 have event:\n{}".format(event1,event2))
+    return event1 + event2
+
+def Makeheader(Data):
+    header = []
+    header0 = [item for item in Data.head(0)]
+    header1 = ["num of events type " + str(i) + " for size 1" for i in range(NUM_OF_FEATURE)]
+    header2 = ["num of events type " + str(i) + " for size 2" for i in range(NUM_OF_FEATURE)]
+    header = header0 + header1 + header2
+    return header
+
+def main():
+    # read ginf data
+    Gintdata = pd.read_csv("ginf.csv")
+    # Making header
+    header = Makeheader(Gintdata)
+    # read event data
+    Eventdata = pd.read_csv("events.csv")
+    #get the fauture from events
+    raws = []
+    #IDs = list(Gintdata["id_odsp"])
+    for raw in Gintdata.values:
+        Eventdatatemp = Eventdata[(Eventdata["id_odsp"] == raw[0])] # raw[0] == ID
+        raws.append(list(raw)+EventToDF(Eventdatatemp))
+
+    res = pd.DataFrame(data=raws, columns=header)
+    res = res.drop(columns=["num of events type 0 for size 1","num of events type 0 for size 2"])
+    res.to_csv("final data.csv")
+
+
+if __name__ == "__main__":
+    main()
